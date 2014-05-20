3ee461140c3adc366a591a59be4cd87c9b1b3987
liveinjs <liveinjs.com@gmail.com>
2014-05-20 16:28:04 +0800

    Create argumentNames.js

---
+++ argumentNames.js
@@ -0,0 +1,8 @@
+//获取函数的参数列表
+
+function argumentNames() {
+    var names = this.toString().match(/^[\s\(]*function[^(]*\(([^)]*)\)/)[1]
+      .replace(/\/\/.*?[\r\n]|\/\*(?:.|[\r\n])*?\*\//g, '')
+      .replace(/\s+/g, '').split(',');
+    return names.length == 1 && !names[0] ? [] : names;
+  }
